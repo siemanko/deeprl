8b3b99b743aeb2a3983de6722b558b753212e050
Szymon Sidor <szymon.sidor@gmail.com>
2016-02-07 16:37:17 -0500

    Can now generate videos

--- asyncrl/simulator/colliding_marbles/karpathy_game.py
+++ asyncrl/simulator/colliding_marbles/karpathy_game.py
@@ -1,6 +1,8 @@
 import os
+import subprocess
 
 from euclid import Vector2
+from progress.bar import Bar
 
 from .colliding_marbles import HeroSimulator
 
@@ -44,8 +46,23 @@ class KarpathyGame(object):
     def save_recording(self, directory):
         assert self.record, "To create a recording pass record=True to the constructor."
 
+        frame_files = []
+        bar = Bar('Saving frames', max=len(self.recording))
         for i, frame in enumerate(self.recording):
-            file_name = 'frame_%09d.svg' % (i,)
+            bar.next()
+            file_path_svg = os.path.join(directory, 'frame_%09d.svg' % (i,))
+            file_path_png = os.path.join(directory, 'frame_%09d.png' % (i,))
 
-            with open(os.path.join(directory, file_name), "wt") as f:
+            with open(file_path_svg, "wt") as f:
                 frame.write_svg(f)
+
+            subprocess.check_output(["rsvg-convert", file_path_svg, "-o", file_path_png,
+                                     "-b", "white"])
+            frame_files.extend([file_path_svg, file_path_png])
+        bar.finish()
+        print("Merging frames...", flush=True)
+        subprocess.check_output(["ffmpeg", "-r", str(self.fps), "-pattern_type", "glob","-i",
+                                 '*.png', "-c:v", "libx264", "video.mp4"],
+                                 stderr=subprocess.DEVNULL, cwd=directory)
+        for file_path in frame_files:
+            os.unlink(file_path)
